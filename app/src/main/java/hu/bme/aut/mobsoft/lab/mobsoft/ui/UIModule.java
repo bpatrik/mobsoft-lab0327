b2a199333daca22cf5e7811e65279b424db148fb
Braun Patrik <bra.patrik@gmail.com>
2017-03-27 10:25:37 +0200

    adding dagger

---
+++ app/src/main/java/hu/bme/aut/mobsoft/lab/mobsoft/ui/UIModule.java
@@ -0,0 +1,31 @@
+package hu.bme.aut.mobsoft.lab.mobsoft.ui;
+
+
+import android.content.Context;
+
+import javax.inject.Singleton;
+
+import dagger.Module;
+import dagger.Provides;
+import hu.bme.aut.mobsoft.lab.mobsoft.ui.main.MainPresenter;
+
+@Module
+public class UIModule {
+    private Context context;
+
+    public UIModule(Context context) {
+        this.context = context;
+    }
+
+    @Provides
+    public Context provideContext() {
+        return context;
+    }
+
+    @Provides
+    @Singleton
+    public MainPresenter provideMainPresenter() {
+        return new MainPresenter();
+    }
+
+}
